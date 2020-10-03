c6267772aef7d596e4cb2217c62deed32d71dd30
Matthias Urlichs <matthias@urlichs.de>
2020-10-03 19:04:41 +0200

    Initial hack

---
+++ README.rst
@@ -0,0 +1,55 @@
+============
+anyio_serial
+============
+
+A small Python wrapper that combines [anyio](https://anyio.readthedocs.io)
+and [pySerial](https://pypi.org/project/pyserial/).
+
+Implementation detail: This library is using too many short-lived threads.
+Yes this should be improved.
+
+Quick start
+===========
+
+A simple serial port reader
++++++++++++++++++++++++++++
+
+::
+   ```py
+   import anyio
+   from anyio_serial import Serial
+
+
+   async def read_and_print(port: Serial):
+      while True:
+         print((await port.read).decode(errors='ignore'), end='', flush=True)
+
+   async def main():
+      async with Serial(port='COM1') as port:
+         await read_and_print(port)
+
+   anyio.run(main)
+
+API
+===
+
+Serial
+++++++
+
+::
+   from anyio_serial import Serial
+
+Constructor
+-----------
+
+::
+   async with Serial(...) as port:  # same options as serial.Serial
+      ...
+      # use "port" like any other anyio ByteStream
+
+Attributes
+----------
+
+The states of the serial status lines ``cd``, ``cts``, ``dsr`` and ``ri``
+are supported.
+
---
+++ anyio_serial/_impl.py
@@ -0,0 +1,72 @@
+# Copyright 2020 Matthias Urlichs
+#
+# License: MIT
+
+import anyio
+import serial
+
+class Serial(anyio.abc.ByteStream):
+    _port = None
+
+    def __init__(self, *a, **kw):
+        self._a = a
+        self._kw = kw
+        self._send_lock = anyio.create_lock()
+        self._receive_lock = anyio.create_lock()
+
+    @asynccontextmanager
+    async def open(self):
+        await anyio.run_sync_in_worker_thread(self._open)
+        try:
+            yield self
+        finally:
+            await self.aclose()
+
+    async def aclose(self):
+        port, self._port = self._port, None
+        if port is None:
+            return
+        await anyio.run_sync_in_worker_thread(self._close, port)
+
+
+    def _open(self):
+        self._port = serial.Serial(*self._a, **self.kw)
+
+    def _close(self, port):
+        port.close()
+
+    async def send_eof(self):
+        pass
+
+    async def receive(self, max_bytes=4096):
+        async with self._receive_lock:
+            return await anyio.run_sync_in_worker_thread(self._read, max_bytes)
+
+    async def send(self, bytes):
+        async with self._send_lock:
+            return await anyio.run_sync_in_worker_thread(self._port.write, bytes)
+
+    def _read(self, max_bytes):
+        p = self._port
+        if not p.in_waiting:
+            return p.read()
+        if p.in_waiting < max_bytes:
+            max_bytes = p.in_waiting
+        return p.read(max_bytes)
+
+    @property
+    def cd(self):
+        return self._port.cd
+
+    @property
+    def dts(self):
+        return self._port.dts
+
+    @property
+    def dsr(self):
+        return self._port.dsr
+
+    @property
+    def ri(self):
+        return self._port.ri
+

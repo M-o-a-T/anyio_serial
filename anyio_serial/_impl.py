0f7f011d482a36fe67ba0c12706aa1307f313ee3
Ganden Schaffner <gschaffner@pm.me>
2024-04-16 00:00:00 -0700

    Fix SyntaxWarning from docstring

--- anyio_serial/_impl.py
+++ anyio_serial/_impl.py
@@ -198,9 +198,8 @@ class Serial(anyio.abc.ByteStream):
         self._port.break_condition = val
 
     async def send_break(self, duration=0.25):
-        """\ 
-        Send break condition. Timed, returns to idle state after given
-        duration.
+        """
+        Send break condition. Timed, returns to idle state after given duration.
         """
         self.break_condition = True
         await anyio.sleep(duration)
